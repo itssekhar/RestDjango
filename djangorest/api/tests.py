c73c17ba17d4ba4cda7dc25db69d82af4db220d3
itssekhar <itssekhar511@gmail.com>
2019-09-21 17:04:56 -0500

    Modified tests for code readability

--- djangorest/api/tests.py
+++ djangorest/api/tests.py
@@ -3,10 +3,10 @@ from .models import Bucketlist
 
 class ModelTestcase(Testcase):
     def setup(self):
-        self.bucketlist_name="Write world class code"
-        self.bucketlist=Bucketlist(name=self.bucketlist_name)
+        self.bucketlist_name = "Write world class code"
+        self.bucketlist = Bucketlist(name=self.bucketlist_name)
     def test_model_can_create_a_bucketlist(self):
-        old_count=Bucketlist.objects.count()
+        old_count = Bucketlist.objects.count()
         self.bucketlist(save)
-        new_count=Bucketlist.object.count()
+        new_count = Bucketlist.object.count()
         self.assertNotEqual(old-count,new_count)
