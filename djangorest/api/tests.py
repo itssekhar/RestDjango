ba278aa8f2dc87c659c3d642d7ae409a32bd4642
itssekhar <itssekhar511@gmail.com>
2019-09-21 16:09:36 -0500

    added tests for Api App

--- djangorest/api/tests.py
+++ djangorest/api/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Bucketlist
 
-# Create your tests here.
+class ModelTestcase(Testcase):
+    def setup(self):
+        self.bucketlist_name="Write world class code"
+        self.bucketlist=Bucketlist(name=self.bucketlist_name)
+    def test_model_can_create_a_bucketlist(self):
+        old_count=Bucketlist.objects.count()
+        self.bucketlist(save)
+        new_count=Bucketlist.object.count()
+        self.assertNotEqual(old-count,new_count)
