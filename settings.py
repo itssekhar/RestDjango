06246d66e4ab22c4cd139e3d472f5d3fb3d58b77
itssekhar <itssekhar511@gmail.com>
2019-09-21 15:53:03 -0500

    Created Djangorest and Api apps

--- settings.py
+++
@@ -1,9 +0,0 @@
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles', # Ensure a comma ends this line
-    'rest_framework', # Add this line
-]
